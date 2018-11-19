4acfff929734bac3b8a14ac8e77a0db473277c33
Dejan <dekimioc@yahoo.com>
2018-11-19 20:12:59 +0100

    Updating state, array immutably, and outsorcing action types

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,25 +1,43 @@
+import * as actionTypes from './actions';
+
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 };
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
-        case "INCREMENT":
+        case actionTypes.INCREMENT:
             return {
+                ...state,
                 counter: state.counter + 1
             };
-        case "DECREMENT":
+        case actionTypes.DECREMENT:
             return {
+                ...state,
                 counter: state.counter - 1
             };
-        case "ADD":
+        case actionTypes.ADD:
             return {
+                ...state,
                 counter: state.counter + action.value
             };
-        case "SUBTRACT":
+        case actionTypes.SUBTRACT:
         return {
+            ...state,
             counter: state.counter - action.value
         }
+        case actionTypes.STORE_COUNTER:
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+            }
+        case actionTypes.DELETE:
+            const updatedArray = state.results.filter(result => result.id !== action.elId);
+            return {
+                ...state,
+                results: updatedArray
+            }
     }
     return state;
 };
